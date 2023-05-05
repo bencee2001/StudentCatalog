2222617ee59ca7fd5ac94e23c5a966f1ea398686
bencee2001 <birben628@gmail.com>
2023-05-05 19:59:08 +0200

    sabkenf liguibase

--- src/main/java/com/syscode/StudentCatalog/entity/Student.java
+++ src/main/java/com/syscode/StudentCatalog/entity/Student.java
@@ -2,8 +2,12 @@ package com.syscode.StudentCatalog;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "student", schema="public")
+@Data
 public class Student {
 
     @Id
