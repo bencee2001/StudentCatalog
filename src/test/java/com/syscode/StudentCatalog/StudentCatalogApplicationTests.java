36cbc99c261bce986280f1ab6dd23637ce07d72b
bencee2001 <birben628@gmail.com>
2023-05-05 17:25:13 +0200

    init

---
+++ src/test/java/com/syscode/StudentCatalog/StudentCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syscode.StudentCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
